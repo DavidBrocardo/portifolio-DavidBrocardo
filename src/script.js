7a107c6d41c0f049436ab16ec29e7ca1b4a2b43e
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2025-08-31 16:17:38 -0300

    Novo repositorio para o portigolio usando vite

---
+++ src/script.js
@@ -0,0 +1,55 @@
+import emailjs from "@emailjs/browser";
+
+// Inicializar EmailJS com sua chave pública
+emailjs.init("cowLwV_xhd0ll5ujj");
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('menuToggle');
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  toggleBtn.addEventListener('click', () => {
+    mobileMenu.style.display =
+    mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+    mobileMenu.classList.toggle("show");
+    toggleBtn.classList.toggle("active");
+  });
+
+  mobileMenu.addEventListener('click', () => {
+    mobileMenu.style.display =
+      mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+        mobileMenu.classList.remove("show");
+      toggleBtn.classList.remove("active"); 
+
+  });
+
+
+const form = document.getElementById("formContato");
+  form.addEventListener("submit", enviarFormulario);
+});
+
+
+function enviarFormulario(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const mensagem = document.getElementById('mensagem').value;
+  const resposta = document.getElementById('resposta');
+
+  emailjs.send("service_46cdjyu", "template_ckx08ov", {
+    name: nome,
+    email: email,
+    message: mensagem
+  }, "cowLwV_xhd0ll5ujj")
+    .then(() => {
+      resposta.textContent = "Enviado com sucesso!";
+      resposta.style.color = "green";
+      document.getElementById('formContato').reset();
+    })
+    .catch((error) => {
+      resposta.textContent = "Erro: " + error.text;
+      resposta.style.color = "red";
+    });
+
+  return false;
+}
